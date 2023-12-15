cdd4b307f4503ec5621e0b9e31c57f9b7c6121e7
Blessing Victor <128801000+Blessingngo@users.noreply.github.com>
2023-12-15 05:05:55 +0100

    monty

--- main.c
+++ main.c
@@ -26,6 +26,7 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
+	bus.file = file;
 	while (read_line > 0)
 	{
 		content = NULL;
